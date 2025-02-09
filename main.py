9b8dc87d72429a6dc4ce8235e8a93084b84ef047
Chase Manning <chase@manning.dev>
2025-02-09 20:05:32 +1300

    :rocket: add more imports to main

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import get_ideal_bond_ratio
+import get_reallocation_strategy
+import get_chart
+import get_ideal_dca_duration
 
 get_ideal_bond_ratio.main()
+# get_reallocation_strategy.main()
+
+# get_ideal_dca_duration.main()
+# get_chart.main("tr_cape", True)
