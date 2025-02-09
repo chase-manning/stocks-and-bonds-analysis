aa86c5c08e269b0534246551f846cb83223fadca
Chase Manning <chase@manning.dev>
2025-02-09 20:04:14 +1300

    :art: refactor to varying investments

--- get_reallocation_strategy.py
+++ get_reallocation_strategy.py
@@ -1,49 +1,55 @@
 import get_shiller_data
 import matplotlib.pyplot as plt
 import pandas as pd
-
-start_amount = float(10_000)
+import locale
 
 data = get_shiller_data.main()
 
-# Normalize the 'cape' data
-cape_values = [obj['cape'] for obj in data]
+scale_key = "tr_cape"
+cape_values = [obj[scale_key] for obj in data]
 min_cape = min(cape_values)
 max_cape = max(cape_values)
 
 for obj in data:
-    obj['cape'] = (obj['cape'] - min_cape) / (max_cape - min_cape)
+    obj[scale_key] = (obj[scale_key] - min_cape) / (max_cape - min_cape)
 
 
-def main(bonds_ratio):
-    stocks_ratio = float(1) - bonds_ratio
-
-    current_amount = start_amount
+def main():
+    # current_amount = 0
     previous = None
+    bonds = 0
+    stocks = 0
     for obj in data:
         if (previous is None):
             previous = obj
             continue
 
         # Calculate the amount of money in stocks and bonds
-        stocks = current_amount * stocks_ratio
-        bonds = current_amount * bonds_ratio
+        bonds_ratio = previous[scale_key]
+        stocks_ratio = float(1) - bonds_ratio
+
+        # Adding monthly investment
+
+        # Calculate the amount of money in stocks and bonds
+        stocks += obj["cpi"] * stocks_ratio
+        bonds += obj["cpi"] * bonds_ratio
 
         # Get percent increase in the stock market
         key = "real_price"
         profit = obj[key] - previous[key]
         stock_increase = profit / previous[key]
-        new_stock_value = stocks * (1 + stock_increase)
+        stocks = stocks * (1 + stock_increase)
 
         # Get percent increase in the bond market
         key = "real_total_bond_returns"
         profit = obj[key] - previous[key]
         bond_increase = profit / previous[key]
-        new_bond_value = bonds * (1 + bond_increase)
+        bonds = bonds * (1 + bond_increase)
 
         # Calculate the new total amount
-        current_amount = new_stock_value + new_bond_value
 
         previous = obj
 
-    return current_amount / start_amount
+    current_amount = stocks + bonds
+    locale.setlocale(locale.LC_ALL, '')
+    print("Total amount: ", locale.currency(current_amount, grouping=True))
