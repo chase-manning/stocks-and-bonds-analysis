1945ed2a2dec68b1ebe2aee3b3659737a7c0da87
Chase Manning <chase@manning.dev>
2025-02-09 15:12:04 +1300

    :rocket: add query for ideal bond ratio

--- main.py
+++ main.py
@@ -1,28 +1,3 @@
-import get_shiller_data
-import matplotlib.pyplot as plt
-import pandas as pd
+import get_ideal_bond_ratio
 
-data = get_shiller_data.main()
-
-print(data[-1]["date_fraction"])
-x = [obj["date_fraction"] for obj in data]
-y = [obj["cape"] for obj in data]
-
-# Create line plot
-plt.plot(x, y, linestyle='-', color='b', label='Line Plot')
-
-# Calculate the Exponential Moving Average (EMA)
-ema_span = 20  # You can adjust the span for the EMA
-y_series = pd.Series(y)
-ema = y_series.ewm(span=ema_span, adjust=False).mean()
-
-# Plot the EMA
-plt.plot(x, ema, linestyle='--', color='r', label='EMA')
-# plt.yscale('log')
-plt.xlabel('X-axis')
-plt.ylabel('Y-axis')
-plt.title('Simple Matplotlib Chart')
-plt.legend()
-
-# Show the plot
-plt.show()
+get_ideal_bond_ratio.main()
