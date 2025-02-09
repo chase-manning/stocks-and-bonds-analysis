e49326777c1d7a8004b38a3d691d4417d1fa28b1
Chase Manning <chase@manning.dev>
2025-02-09 14:07:02 +1300

    :rocket: add simple chart

---
+++ main.py
@@ -0,0 +1,19 @@
+import get_shiller_data
+import matplotlib.pyplot as plt
+
+data = get_shiller_data.main()
+
+x = [obj["date_fraction"] for obj in data]
+y = [obj["sp_composite_price"] for obj in data]
+
+# Create line plot
+plt.plot(x, y, marker='o', linestyle='-', color='b', label='Line Plot')
+
+# Adding labels and title
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Simple Matplotlib Chart')
+plt.legend()
+
+# Show the plot
+plt.show()
